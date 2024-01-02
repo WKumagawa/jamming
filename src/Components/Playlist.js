663a29aa308595686d736ad25b859ade5f5b16d0
Will Kumagawa <williamkumagawa@gmail.com>
2024-01-02 16:09:38 -0700

    Base Functionality tested and working

---
+++ src/Components/Playlist.js
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+
+
+
+import TrackList from './Tracklist';
+
+const Playlist =(props) => {
+    const handleNameChange = useCallback(
+        (event) => {
+            props.onNameChange(event.target.value);
+        },
+        [props.onNameChange]
+    );
+
+    return (
+        <div className="Playlist">
+            <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+            <TrackList
+              tracks={props.playlistTracks}
+              isRemoval={true}
+              onRemove={props.onRemove}
+              />
+            <button className="Plalist-save" onClick={props.onSave}>
+                SAVE TO SPOTIFY
+            </button>
+        </div>
+    );
+};
+
+export default Playlist;
